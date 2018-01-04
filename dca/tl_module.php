9406b68c3635f63b66ad70212a454ca69c496c8e
fbliss <fred.bliss@intelligentspark.com>
2018-01-04 11:11:37 -0500

    fixed label, moved field up above numberOfItems field

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -3,12 +3,12 @@
 
 #$GLOBALS['TL_DCA']['tl_module']['palettes']['newslist']    = '{title_legend},name,headline,type;{config_legend},news_archives,numberOfItems,news_featured,perPage,skipFirst;{template_legend:hide},news_metaFields,news_template,customTpl;{image_legend:hide},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['newslist'] = str_replace('news_featured,','news_featured,news_sticky,', $GLOBALS['TL_DCA']['tl_module']['palettes']['newslist']);
+$GLOBALS['TL_DCA']['tl_module']['palettes']['newslist'] = str_replace('numberOfItems,','news_sticky,numberOfItems,', $GLOBALS['TL_DCA']['tl_module']['palettes']['newslist']);
 
 
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['news_sticky'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['sticky'],
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['news_sticky'],
     'exclude'                 => true,
     'inputType'               => 'checkbox',
     'eval'                    => array(),
